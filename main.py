95ea0cd6d8afc8834f94a59e48faff9275eef8e5
Rebyc <5609903+cutipu@users.noreply.github.com>
2023-03-05 16:30:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 from twilio.rest import Client
 
-# setup
+# Thiết lập thông tin tài khoản Twilio
 account_sid = ''
 auth_token = ''
 client = Client(account_sid, auth_token)
 
 # Số điện thoại nhận tin nhắn
-#to_number = input("\nsdt: ")
+to_number = input("\nsdt: ")
 
 # Số điện thoại của tài khoản Twilio
 from_number = ''
@@ -15,14 +15,10 @@ from_number = ''
 # Nội dung tin nhắn
 message = input("\nNoi Dung: ")
 
-with open('sdt.txt') as f:
-    numbers = f.readlines()
-numbers = [x.strip() for x in numbers]
+# Gửi tin nhắn
+message = client.messages.create(
+    to=to_number,
+    from_=from_number,
+    body=message)
 
-# send all list
-for to_number in numbers:
-    message = client.messages.create(
-        to=to_number,
-        from_=from_number,
-        body=message)
-    print('Message sent successfully to', to_number)
+print('Message sent successfully to', to_number)
